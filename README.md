ed22501307146fb7132f5949cca5e7c2c91ecf17
David Forero <davefogo@gmail.com>
2020-10-12 15:42:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,45 +1,31 @@
-### WebdriverIO-v6 boilerplate code with Mocha BDD
+### Third-party Rater Tests 
 
-This repository contains a collection of sample webdriverIO (v6x) projects and libraries that demonstrate how to use the tool and develop automation script using the Mocha BDD framework. It uses the `chromedriver` NPM package that wraps the ChromeDriver for you. This service does not require a Selenium server, but uses ChromeDriver to communicate with the browser directly.
+This repository contains the UI tests that go through insurestation. It uses the `chromedriver` NPM package that wraps the ChromeDriver for you. This service does not require a Selenium server, but uses ChromeDriver to communicate with the browser directly.
 
 It support ES6 (via babel-register) and uses Grunt to manage tasks, provides utilities to read data from MS-Excel, executes SQL statements to any database(RDBMS such as Oracle, TeraData, MySQL, Vertica) for end to end testing. It generate Spec, JUNIT, Allure reporters as well.
-
-💡 If you need the wdio-v5 boilerplate project, please take the code from v5 branch: click [here](https://github.com/amiya-pattnaik/webdriverIO-with-mochaBDD/tree/wdio-v5)
 
 
 ### Installation
 This project is tested on **Node v12.0.0** and above.  While earlier versions of node may be compatible, but they have not been verified.
 
+
 `Node.JS:` Install  from the site - https://nodejs.org/en/  take the LTS version based on your Operating system. Please make sure you install NodeJS globally. To take full advantage of the command line and use grunt tasks you will need to make sure that you have added `node_modules/.bin` to your `$PATH`.  Otherwise you will need to install `npm install -g  grunt-cli` globally.
 
 
-`JDK 1.8:` It is optional, install JDK 1.8+ and make sure class path is set properly. JAVA is require to start `Selenium Server` on your local environment nothing else.
-
-
-### Selenium Tests / Appium Tests
-
-  To run your test you must have selenium / Appium server up and running to execute any webdriverIO tests, or it will fail fast with an error. To start selenium automatically it has been added as part of `services: ['selenium-standalone']` and `services: ['appium']` in the *.conf.js.  That's all there is to it.!.
+`JDK 1.8:` It is optional, install JDK 1.8+ and make sure class path is set properly. JAVA is required to start `Selenium Server` on your local environment nothing else.
 
 ### Run Some Sample Tests
 
 To execute the entire test suite in local development, you can use any one of the options mentioned below
 
-Option 1: `npm run test-local`. You can also run in SauceLabs and BrowserStack using `npm run test-sauce`, `npm run test-browserstack`.
+Option 1: `npm run test-local`. 
 
 Option 2: `grunt webdriver:test-local`.  This executes all spec files in the [`./test/specs/*.js`] directory.
-The default option for Grunt run is `webdriver:test-local`. But you can use `webdriver:test-sauce` or `test-browserstack` based on your requirements.
-
-To execute tests on `mobile device` use : `npm run test-mobile`.
-
-💡 Before running mobile tests, perform the requisite Appium setup. For hassle free `one click Appium setup on OSX` refer [appium-setup-made-easy-OSX](https://github.com/amiya-pattnaik/appium-setup-made-easy-OSX) or refer [Appium Docs](http://appium.io/getting-started.html?lang=en)
+The default option for Grunt run is `webdriver:test-local`. But you can use `test-local` based on your requirements.
 
 ### Config Files
 
 WebdriverIO uses configuration files to setup and execute tests in specific ways.  The configuration is fully customizable, and different functions can be invoked before, during and after each test or test suite.  Config files are found in the `/test/config/` directory and all end with `*.conf.js`.  These can be called via the the cli
-
-### SauceLabs / BrowserStack Integration
-
-`SauceLabs` and `BrowserStack` specific code has been added in the `wdio.sauce.conf.js` and  `wdio.browserstack.conf.js` under the /test/config folder. You just need to provide your SauceLabs/BrowserStack credentials in the config file. To run test on SauceLabs, execute command `npm run test-sauce` and on BrowserStack `npm run test-browserstack`.
 
 ### Logs  
 
---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,142 @@
+
+import Page from './page';
+
+class loginPage extends Page  {
+  /**
+  * define elements
+  */
+
+  get loginButton()  { return $('#ctl00_body_LoginButton'); }
+  get welcomeScreen() { return $('.welcomeContainer'); }
+  get rateQuoteButton() { return $('[name="ctl00$CPH$RateQuoteButton"]'); }
+  get secondPageRateQuoteButton() { return $('#CPH_RateButton'); }
+  get bridgeButton() { return $('#CPH_ComparisonGrid_BridgeQuoteLink_0'); }
+  get okButton() { return $('#CPH_MessagesOKButton'); }
+  get product() { return $('#integrationPopup_integrationLinkGridCarriers_IntegrationHyperLink_0'); }
+  
+  inputFields(field) {
+    let fields, selectedField
+    
+    fields = {
+      'userInput': () => { return $('#ctl00_body_UserNameEntry'); },
+      'passwordInput': () => { return $('#ctl00_body_PasswordEntry'); },
+      'firstNameInput': () => { return $('#CPH_InsuredFirstNameEntry'); },
+      'lastNameInput': () => { return $('#CPH_InsuredLastNameEntry'); },
+      'addressInput': () => { return $('#CPH_InsuredAddressEntry'); },
+      'secondaryAddressInput': () => { return $('[name="ctl00$CPH$InsuredAddress2Entry"]'); },
+      'zipCodeInput': () => { return $('#CPH_ZipCodeEntry'); },
+      'countyDropdown': () => { return $('[name="ctl00$CPH$CountyEntry"]'); },
+      'timeAtResidenceInput': () => { return $('#CPH_TimeAtResidenceEntry_YearsEntry'); },
+      'effectiveDateInput': () => { return $('#CPH_effDateEntry_DateEntry'); },
+      'liabilityDropdown': () => { return $('#CPH_ctl61_LiabBILimitsEntry'); },
+      'dobInput': () => { return $('[name="ctl00$CPH$driverRepeater$ctl00$DriverDOBEntry$DateEntry"]'); },    
+      'genderDropdown': () => { return $('#CPH_driverRepeater_DriverSexEntry_0'); },    
+      'maritalDropdown': () => { return $('[name="ctl00$CPH$driverRepeater$ctl00$DriverMaritalEntry"]'); },    
+      'priorTimeInput': () => { return $('[name="ctl00$CPH$driverRepeater$ctl00$PriorCoverageEntry$YearsEntry"]'); },
+      'priorExpirationDateInput': () => { return $('[name="ctl00$CPH$driverRepeater$ctl00$PriorExpDateEntry$DateEntry"]'); },
+      'vinInput': () => { return $('[name="ctl00$CPH$CarRepeater$ctl00$CarVINEntry"]'); },
+      'milesInput': () => { return $('[name="ctl00$CPH$CarRepeater$ctl00$AnnualMilesEntry"]'); },      
+    }    
+    selectedField = fields[field];
+
+    return selectedField();
+  }
+
+  /**
+   * page methods
+   */
+  bridge() {
+    this.bridgeButton.waitForDisplayed();
+    this.bridgeButton.scrollIntoView();
+    this.bridgeButton.waitForEnabled();
+    this.bridgeButton.click();
+    browser.pause(1000);
+  }
+
+  checkInput(inputField) {
+    return this.inputFields(inputField).getValue();
+  }
+
+  clickOkButton() {
+    this.okButton.waitForDisplayed();
+    this.okButton.waitForEnabled();
+    this.okButton.click();
+  }
+
+  enterText(inputField, value) {
+    this.inputFields(inputField).waitForEnabled();
+    this.inputFields(inputField).setValue(value);
+    browser.pause(2000);
+  }
+  
+  enterTextPassword(inputField, value) {
+    browser.keys(['Tab']);
+    browser.pause(1000);
+    this.inputFields(inputField).setValue(value);
+    browser.pause(2000);
+  }
+
+  logIn() {
+    this.loginButton.click();
+  }
+
+  loggedIn() {
+    this.welcomeScreen.waitForDisplayed(1000);
+    return this.welcomeScreen.isDisplayed();
+  }
+
+  open() {
+    super.open('https://insurestation.com');
+    browser.pause(1000);
+  }
+
+  rateQuote() {
+    browser.pause(1000);
+    this.rateQuoteButton.waitForDisplayed();
+    this.rateQuoteButton.scrollIntoView();
+    this.rateQuoteButton.waitForEnabled();
+    this.rateQuoteButton.click();
+    browser.pause(2000);
+  }
+
+  secondPageRateQuote() {
+    this.secondPageRateQuoteButton.waitForDisplayed();
+    this.secondPageRateQuoteButton.scrollIntoView();
+    this.secondPageRateQuoteButton.waitForEnabled();
+    this.secondPageRateQuoteButton.click();
+    browser.pause(1000);
+  }
+
+  selectFromDropdown(inputField, value) {
+    browser.keys(['Tab'])
+    this.inputFields(inputField).selectByVisibleText(value);
+    browser.keys(['Tab'])
+    browser.pause(1000);
+  }
+
+  selectProduct() {
+    this.product.waitForDisplayed();
+    this.product.waitForEnabled();
+    this.product.click();
+    browser.pause(1000);
+  }
+
+  swloginhTab() {
+    browser.pause(1000);
+    browser.swloginhWindow('https://sprinters.live.ptsapp.com/default.cfm');
+    browser.pause(1000);
+  }
+
+  today() {
+    let date = new Date().getMonth() + 1 > 9 ? '' : '0';
+    date += new Date().getMonth() + 1;
+    date += new Date().getDate() > 9 ? '' : '0';
+    date += new Date().getDate();
+    date += new Date().getFullYear();
+
+    return date
+  }
+
+}
+
+export default new loginPage();
